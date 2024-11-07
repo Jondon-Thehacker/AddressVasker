104f22d7118f6f4ac97b678bfb817b1bdb5e97ae
Jonathan <jonathan.max.michelsen@hotmail.com>
2024-11-07 13:25:20 +0100

    Added local api project asp.net

---
+++ LocalAPI/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+namespace MyLocalApi.Controllers
+
+{
+    [ApiController]
+    [Route("{*url}")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public HelloController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var filePath = Path.Combine(_env.ContentRootPath, "C:/Users/Jonat/source/repos/AddressVasker/LocalAPI/Controllers/response.txt");
+            string jsonContent = System.IO.File.ReadAllText(filePath);
+            return Content(jsonContent, "application/json");
+        }
+    }
+}
---
+++ LocalAPI/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Use CORS
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
