c9d47f983fbff5802660eba0fcaaade4b7bcd96a
Jonathan <jonathan.max.michelsen@hotmail.com>
2024-11-15 15:51:27 +0100

    Integration testing

---
+++ Adressevasker.IntegrationTests/LocalAPITests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Threading;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Adressevasker.IntegrationTests
+{
+    [TestClass]
+    public class LocalAPITests
+    {
+        private Process _apiProcess;
+        private HttpClient _httpClient;
+
+        [TestInitialize]
+        public void StartApi()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "run --project C:/Users/Jonat/source/repos/AddressVasker/LocalAPI/LocalAPI.csproj",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            _apiProcess = Process.Start(startInfo);
+
+            Thread.Sleep(10000); 
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://10.102.0.65:5128")
+            };
+        }
+
+        [TestCleanup]
+        public void StopApi()
+        {
+            if (_apiProcess != null && !_apiProcess.HasExited)
+            {
+                _apiProcess.Kill();
+            }
+        }
+
+        [TestMethod]
+        public async Task Test_ApiEndpoint_PrintJsonResponse()
+        {
+            // Arrange
+            var response = await _httpClient.GetAsync("/test");
+            var json = await response.Content.ReadAsStringAsync();
+
+            // Act
+            Console.WriteLine("API Response:");
+            Console.WriteLine(json);
+
+            // Assert
+            StringAssert.Contains(json, "0a3f50a1-52f0-32b8-e044-0003ba298018");
+        }
+
+    }
+}
---
+++ Adressevasker.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Adressevasker.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Adressevasker.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93d99159-78a7-46aa-8360-781410bed79c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
